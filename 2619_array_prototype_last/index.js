23f07ec6ced31d8ce6d210ae89d641cd159982bc
Quoc Cuong <quoccuong14012000@gmail.com>
2024-02-29 15:37:57 +0700

    Done: problem 2619

---
+++ 2619_array_prototype_last/index.js
@@ -0,0 +1,19 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+  if (!this.length) {
+      return -1;
+  }
+
+  return this[this.length -1]
+};
+
+/**
+* const arr = [1, 2, 3];
+* arr.last(); // 3
+*/
+
+const arr = [1, 2, 3];
+
+console.log(arr.last());
